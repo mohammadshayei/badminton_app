56d93b3e969ae6605a77ac403ac3230f55458624
mohammadshayei <mmdshayei78@gmail.com>
2021-11-16 10:53:08 +0330

    configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    < Routes>
+        {/* <Route path="/example" exact component={example}></Route> */}
+    </ Routes>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+// import exampleReducer from "./store/reducers/example";
+
+
+const rootReducer = combineReducers({
+  // example: exampleReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter basename="/">
+      {/* <ThemeProvider> */}
+        <App />
+      {/* </ThemeProvider> */}
+    </BrowserRouter>
+  </Provider>
+);
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>{app}</React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
