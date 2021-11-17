28ddafc3191bb40a70c33a2c9033b3e11ce48c33
lrdhosssein <77486783+lrdhosssein@users.noreply.github.com>
2021-11-17 12:32:39 +0330

    page 2 of setup

--- src/containers/SetupPage/Page2/Page2.jsx
+++ src/containers/SetupPage/Page2/Page2.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Page2.scss";
+import Input from "../../../components/UI/Input/Input";
+import Button from "../../../components/UI/Button/Button";
 
-const Page2 = () => {
-  return <div>page 2</div>;
+const Page2 = (props) => {
+  return (
+    <div className="page-container">
+      <div className="players">
+        {[1, 2, 3, 4].map(() => (
+          <div className="player-info">
+            <p className="player-name">بازیکن</p>
+            <div className="player-pic"></div>
+            <Input></Input>
+          </div>
+        ))}
+      </div>
+      <Button ButtonStyle={{ padding: ".75em 3em" }}>شروع بازی</Button>
+      <Button onClick={() => props.pageSelector(0)}>قبلی</Button>
+    </div>
+  );
 };
 
 export default Page2;
