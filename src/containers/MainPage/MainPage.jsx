dde35679506cf83200bf7c3a37d1b678aa4e52fe
lrdhosssein <77486783+lrdhosssein@users.noreply.github.com>
2021-11-23 17:09:39 +0330

    clear responsiveness calculations with js

--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -1,56 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import './MainPage.scss'
-import { useTheme } from '../../styles/ThemeProvider';
-import ScoreBoard from './ScoreBoard/ScoreBoard';
-import { useSelector } from 'react-redux';
-import FooterMainPage from './FooterMainPage/FooterMainPage'
-import HeaderMainPage from './HeaderMainPage/HeaderMainPage';
+import React, { useState } from "react";
+import "./MainPage.scss";
+import { useTheme } from "../../styles/ThemeProvider";
+import ScoreBoard from "./ScoreBoard/ScoreBoard";
+import FooterMainPage from "./FooterMainPage/FooterMainPage";
+import HeaderMainPage from "./HeaderMainPage/HeaderMainPage";
+
 const MainPage = () => {
-    const [clicked, setClicked] = useState(false)
-    const [screenOrientation, setScreenOrientation] = useState('portrait')
-    const themeState = useTheme();
-    const theme = themeState.computedTheme;
-    const menuButonClickHandler = () => setClicked(!clicked);
-    const detail = useSelector(state => state.detail)
-    const isPortraitMode = () => {
-        return screenOrientation === 'portrait';
-    }
-    const setScreenOrientationFunciton = () => {
-        if (window.matchMedia("(orientation: portrait)").matches) {
-            setScreenOrientation('landscape')
-        }
-        else if (window.matchMedia("(orientation: landscape)").matches) {
-            setScreenOrientation('portrate')
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('orientationchange', setScreenOrientationFunciton);
-    }, [])
-    useEffect(() => {
-        // console.log(screenOrientation)
-    }, [screenOrientation])
-    if(detail.widthMode !== 1 && detail.heightMode > 1)console.log('real?')
-    console.log(detail.widthMode)
-    return (
-        <div
-            className='main-page-container'
-            style={{ backgroundColor: theme.background_color }}
-        >
-            <div className='menu-icon' onClick={menuButonClickHandler}>
-                <i style={{ color: theme.on_primary }}
-                    className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
-            </div>
-            {
-                detail.widthMode !== 1 && detail.heightMode > 1 && <HeaderMainPage />
+  const [clicked, setClicked] = useState(false);
+  const themeState = useTheme();
+  const theme = themeState.computedTheme;
+  const menuButonClickHandler = () => setClicked(!clicked);
 
-            }
-            <ScoreBoard header={detail.heightMode === 1} isVertical={detail.widthMode === 1} />
-            {
-                detail.heightMode === 3 && <FooterMainPage />
-            }
+  return (
+    <div
+      className="main-page-container"
+      style={{ backgroundColor: theme.background_color }}
+    >
+      <div className="menu-icon" onClick={menuButonClickHandler}>
+        <i
+          style={{ color: theme.on_primary }}
+          className={clicked ? "fas fa-times" : "fas fa-bars"}
+        />
+      </div>
+      <HeaderMainPage />
+      <ScoreBoard />
+      <FooterMainPage />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default MainPage
+export default MainPage;
--- src/containers/MainPage/ScoreBoard/DetailScoreBoard/DetailScoreBoard.jsx
+++ src/containers/MainPage/ScoreBoard/DetailScoreBoard/DetailScoreBoard.jsx
@@ -1,81 +1,61 @@
-import React, { } from 'react'
-import { useSelector } from 'react-redux';
-import { useTheme } from '../../../../styles/ThemeProvider';
-import './DetailScoreBoard.scss'
+import React from "react";
+import { useTheme } from "../../../../styles/ThemeProvider";
+import "./DetailScoreBoard.scss";
+
 const ResultScoreBoard = (props) => {
-    const themeState = useTheme();
-    const theme = themeState.computedTheme;
-    const detail = useSelector(state => state.detail)
+  const themeState = useTheme();
+  const theme = themeState.computedTheme;
 
-    return (
-        <div className="detail-scoreboard-wrapper"
-            style={{
-                height: detail.widthMode === 1 ? props.smallBoxSize : 'calc( 100% - 50px )',
-                width: detail.widthMode === 1 ? 48 + props.boxSize : props.boxSize + 8,
-                paddingTop: detail.widthMode === 1 ? '' : '.5rem',
-                fontWeight: 'bold'
+  return (
+    <div className="detail-scoreboard-wrapper">
+      <p
+        className="detail-scoreboard-time"
+        style={{
+          textAlign: "center",
+          fontSize: "5rem",
+          lineHeight: "4rem",
+          color: "red",
+        }}
+      >
+        {props.time}
+      </p>
 
+      <div className="detail-scorebaord-container">
+        <div
+          className="detail-scorebaord"
+          style={{
+            backgroundColor: theme.background_color,
+          }}
+        >
+          <p>{props.teamAScore}</p>
+        </div>
+        <div
+          className="detail-scorebaord"
+          style={{
+            backgroundColor: theme.background_color,
+          }}
+        >
+          <p>{props.teamBScore}</p>
+        </div>
+      </div>
+      <div className="detail-scoreboard-content">
+        <p className="detail-scoreboard-time">{props.time}</p>
 
-            }}>
-            {
-                detail.widthMode !== 1 && props.time &&
-                <p className='detail-scoreboard-time'
-                    style={{
-                        textAlign: "center",
-                        fontSize: "5rem",
-                        lineHeight: "4rem",
-                        color: "red"
-                    }}>
-                    {props.time}
-                </p>
+        <p
+          className="detail-scoreboard-title"
+          style={{ color: theme.on_primary }}
+        >
+          بازی سوم
+        </p>
+        <p
+          className="detail-scoreboard-title"
+          style={{ color: theme.on_primary }}
+        >
+          شماره زمین : 3
+        </p>
+      </div>
+    </div>
+  );
+};
 
-            }
-            <div className="detail-scorebaord-container"
-                style={{
-                    flexDirection: detail.widthMode === 1 ? 'column' : 'row',
-                }}
-            >
-                <div className="detail-scorebaord"
-                    style={{
-                        backgroundColor: theme.background_color,
-                        height: props.smallBoxSize / 2,
-                        width: props.smallBoxSize / 2,
-                        maxWidth: props.boxSize / 2,
-                        maxHeight: props.boxSize / 2,
-
-                    }}>
-                    <p>{props.teamAScore}</p>
-                </div>
-                <div className="detail-scorebaord"
-                    style={{
-                        backgroundColor: theme.background_color,
-                        marginTop: ".2rem",
-                        height: props.smallBoxSize / 2,
-                        width: props.smallBoxSize / 2,
-                        maxWidth: detail.widthMode !== 1 ? props.boxSize / 2 : '',
-                        maxHeight: detail.widthMode !== 1 ? props.boxSize / 2 : '',
-                        marginRight: detail.widthMode !== 1 ? '.5rem' : '',
-
-
-                    }} >
-                    <p>{props.teamBScore}</p>
-                </div>
-            </div>
-            <div className="detail-scoreboard-content">
-                {
-                    detail.widthMode === 1 && props.time &&
-                    <p className='detail-scoreboard-time'>
-                        {props.time}
-                    </p>
-
-                }
-                <p className='detail-scoreboard-title'
-                    style={{ color: theme.on_primary }}>بازی سوم</p>
-                <p className='detail-scoreboard-title' style={{ color: theme.on_primary }}>شماره زمین : 3</p>
-
-            </div>
-        </div >
-    )
-}
-
-export default ResultScoreBoard
+export default ResultScoreBoard;
