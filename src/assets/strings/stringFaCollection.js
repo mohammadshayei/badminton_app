bd7c2cafd7b89c04c569548eeedb62b6524e932a
mohammadshayei <mmdshayei78@gmail.com>
2021-11-30 10:48:32 +0330

    edited by mohammad but must to change some page

---
+++ src/assets/strings/stringFaCollection.js
@@ -0,0 +1,16 @@
+export const stringFa = {
+    refereee: 'داور بازی',
+    refereee_error: 'نام داور بازی باید بیشتر از 2 حرف باشد',
+    referee_service: "داور سرویس",
+    referee_service_error: 'نام داور سرویس باید بیشتر از 2 حرف باشد',
+    competition_number: 'شماره مسابقه',
+    competition_number_error: 'شماره مسابقه را با اعداد پر کنید',
+    land_number: "شماره زمین",
+    land_number_error: 'شماره زمین را با اعداد پر کنید',
+    game_type: 'نوع بازی',
+    next:"بعدی"
+
+
+
+
+}
--- src/containers/SetupPage/Page1/Page1.jsx
+++ src/containers/SetupPage/Page1/Page1.jsx
@@ -1,17 +1,72 @@
 import React, { useState } from "react";
 import "./Page1.scss";
-import Input from "../../../components/UI/Input/Input";
 import Button from "../../../components/UI/Button/Button";
 import rocket from "../../../assets/images/rocket.png";
 import rockets from "../../../assets/images/rockets.png";
 import { animated, useSpring, useTransition } from "react-spring";
 import { useTheme } from "../../../styles/ThemeProvider";
-import { HiCamera } from "react-icons/hi";
+import { stringFa } from "../../../assets/strings/stringFaCollection";
+import CustomInput from "./CustomInput/CustomInput";
 
 const Page1 = (props) => {
   const themeState = useTheme();
   const theme = themeState.computedTheme;
   const [double, setDouble] = useState(false);
+  const [order, setOrder] = useState({
+    refereee: {
+      value: '',
+      title: stringFa.refereee,
+      error: stringFa.refereee_error,
+      withImage: true,
+      imagePath: "",
+      isValid: true,
+      validation: {
+        isRequired: true,
+        minLength: 3,
+      },
+      isFocused: false,
+    },
+    serviceReferee: {
+      value: '',
+      title: stringFa.refereee,
+      error: stringFa.refereee_error,
+      withImage: true,
+      imagePath: "",
+      isValid: true,
+      validation: {
+        isRequired: true,
+        minLength: 3,
+      },
+      isFocused: false,
+    },
+    competitionNumber: {
+      value: '',
+      title: stringFa.competition_number,
+      error: stringFa.competition_number_error,
+      withImage: false,
+      imagePath: "",
+      isValid: true,
+      validation: {
+        isRequired: true,
+        isNumeric: true,
+      },
+      isFocused: false,
+    },
+    landNumber: {
+      value: '',
+      title: stringFa.land_number,
+      error: stringFa.land_number_error,
+      withImage: false,
+      imagePath: "",
+      isValid: true,
+      validation: {
+        isRequired: true,
+        isNumeric: true,
+      },
+      isFocused: false,
+    },
+  })
+  const [formIsValid, setFormIsValid] = useState(false)
   const styles = useSpring({
     from: { left: double ? "0" : "51px" },
     to: { left: double ? "51px" : "0" },
@@ -23,36 +78,63 @@ const Page1 = (props) => {
     leave: { transform: "translateX(-20%)" },
   });
 
+  const checkValidaty = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern =
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+    return isValid;
+  };
+  const onChange = (e, key) => {
+    let updatedOrder = { ...order };
+    let upadtedElement = updatedOrder[key];
+    upadtedElement.value = e.target.value;
+    upadtedElement.isValid = checkValidaty(
+      e.target.value,
+      upadtedElement.validation
+    );
+    updatedOrder[key] = upadtedElement;
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrder) {
+      formIsValid = updatedOrder[inputIdentifier].isValid && formIsValid;
+    }
+    setFormIsValid(formIsValid)
+    setOrder(updatedOrder)
+  };
+
   return transition((tranStyles, item) => (
     <animated.div className="page-container page-1" style={tranStyles}>
-      <div className="input-line">
-        <img className="camera" src="https://i.pravatar.cc/126" alt="avatar" />
-        <div className="avatar">
-          <div className="avatar-upload-btn">
-            <HiCamera />
-          </div>
-        </div>
-        <Input></Input>
-        <p> داور بازی</p>
-      </div>
-      <div className="input-line">
-        <img className="camera" src="https://i.pravatar.cc/127" alt="avtar" />
-        <div className="avatar">
-          <div className="avatar-upload-btn">
-            <HiCamera />
-          </div>
-        </div>
-        <Input></Input>
-        <p> داور سرویس</p>
-      </div>
-      <div className="input-line">
-        <Input></Input>
-        <p>شماره مسابقه</p>
-      </div>
-      <div className="input-line">
-        <Input></Input>
-        <p>شماره زمین</p>
-      </div>
+      {
+        Object.entries(order).map(([k, v]) =>
+          <CustomInput
+            key={k}
+            info={v}
+            onChange={(e) => onChange(e, k)}
+          />)
+      }
       <div className="input-line full-row">
         <div className="width-fixer">
           <animated.div
@@ -65,7 +147,7 @@ const Page1 = (props) => {
           <img src={rocket} alt="rocket" onClick={() => setDouble(false)} />
           <img src={rockets} alt="rocket" onClick={() => setDouble(true)} />
         </div>
-        <p>نوع بازی</p>
+        <p>{stringFa.game_type}</p>
       </div>
       <Button
         className="next-btn"
@@ -73,7 +155,7 @@ const Page1 = (props) => {
           props.pageSelector(1);
         }}
       >
-        بعدی
+        {stringFa.next}
       </Button>
     </animated.div>
   ));
