3a601d2d46e6eccbd084799e8a6ef32a89fdef73
lrdhosssein <77486783+lrdhosssein@users.noreply.github.com>
2021-12-06 11:13:44 +0330

    edit for make more simillar to design

--- src/containers/HomePage/GamesPage/GamesPage.jsx
+++ src/containers/HomePage/GamesPage/GamesPage.jsx
@@ -3,9 +3,9 @@ import "./GamesPage.scss";
 import { useTheme } from "../../../styles/ThemeProvider";
 import arrow from "../../../assets/images/arrow.png";
 import { stringFa } from "../../../assets/strings/stringFaCollection.js";
-import { BsThreeDots } from "react-icons/bs";
 import rocket from "../../../assets/images/rocket.png";
 import rockets from "../../../assets/images/rockets.png";
+import { GoSettings } from "react-icons/go";
 
 const GamesPage = () => {
   const [games, setGames] = useState([]);
@@ -19,22 +19,15 @@ const GamesPage = () => {
         tournament: "مسابقات دهه فجر 1400",
         number: "5",
         type: "single",
-        player1: "تقی",
-        player2: "حسن",
+        player1: "احمد حسنی",
+        player2: "حمید کاظمی",
       },
       {
         tournament: "مسابقات دهه فجر 1400",
         number: "5",
         type: "single",
-        player1: "تقی",
-        player2: "حسن",
-      },
-      {
-        tournament: "مسابقات دهه فجر 1400",
-        number: "5",
-        type: "single",
-        player1: "تقی",
-        player2: "حسن",
+        player1: "احمد حسنی",
+        player2: "حمید کاظمی",
       },
     ]);
   }, []);
@@ -51,10 +44,10 @@ const GamesPage = () => {
             )}
             <div className="details">
               <p className="title">{item.tournament}</p>
-              <p className="game-number">{`${item.number} ${stringFa.game_number}`}</p>
+              <p className="game-number">{`${stringFa.game_number} ${item.number}`}</p>
               <p className="players-name">{`${item.player1} - ${item.player2}`}</p>
             </div>
-            <BsThreeDots />
+            <GoSettings fontSize="1.5rem" />
           </div>
         ))
       ) : (
