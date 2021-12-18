9539baa0072009e2eb589a3cdfae08bf1c0e5e59
lrdhosssein <77486783+lrdhosssein@users.noreply.github.com>
2021-12-18 10:47:56 +0330

    first setups for start game

--- src/containers/HomePage/GamesPage/GamesPage.jsx
+++ src/containers/HomePage/GamesPage/GamesPage.jsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./GamesPage.scss";
-import { useTheme } from "../../../styles/ThemeProvider";
 import arrow from "../../../assets/images/arrow.png";
 import { stringFa } from "../../../assets/strings/stringFaCollection.js";
 import rocket from "../../../assets/images/rocket.png";
 import rockets from "../../../assets/images/rockets.png";
 import { GoSettings } from "react-icons/go";
+import Modal from "../../../components/UI/Modal/Modal"
+import Selector from "./Selector/Selector";
+import * as infoActions from "../../../store/actions/setInfo"
+import { useDispatch } from "react-redux";
 
 const GamesPage = () => {
   const [games, setGames] = useState([]);
-  const themeState = useTheme();
-  const theme = themeState.computedTheme;
+  const [showModal, setShowModal] = useState(false);
+
+  const dispatch = useDispatch();
+  const setScoreboard = (data) => {
+    dispatch(infoActions.setScoreboardData(data));
+  };
 
   useEffect(() => {
     setGames([
@@ -19,24 +26,35 @@ const GamesPage = () => {
         tournament: "مسابقات دهه فجر 1400",
         number: "5",
         type: "single",
-        player1: "احمد حسنی",
-        player2: "حمید کاظمی",
+        playerA1: "احمد حسنی",
+        playerB1: "حمید کاظمی",
       },
       {
         tournament: "مسابقات دهه فجر 1400",
         number: "5",
-        type: "single",
-        player1: "احمد حسنی",
-        player2: "حمید کاظمی",
+        type: "double",
+        playerA1: "احمد حسنی",
+        playerA2: "مرتضی شکوری زاده",
+        playerB1: "محمدحسن حسینی",
+        playerB2: "حمید کاظمی"
       },
     ]);
   }, []);
 
+  const gameClickHandler = (i) => {
+    setScoreboard(games[i]);
+    setShowModal(true);
+  }
+
   return (
     <div className="games-page-wrapper">
+      {showModal &&
+        <Modal show={showModal} modalClosed={() => setShowModal(false)}>
+          <Selector show={setShowModal} />
+        </Modal>}
       {games.length > 0 ? (
         games.map((item, key) => (
-          <div key={key} className="game-box">
+          <div key={key} className="game-box" onClick={() => gameClickHandler(key)}>
             {item.type === "single" ? (
               <img src={rocket} alt="" />
             ) : (
@@ -45,7 +63,12 @@ const GamesPage = () => {
             <div className="details">
               <p className="title">{item.tournament}</p>
               <p className="game-number">{`${stringFa.game_number} ${item.number}`}</p>
-              <p className="players-name">{`${item.player1} - ${item.player2}`}</p>
+              {<p className="players-name">
+                {`${item.playerA1}
+                 ${item.type === "double" ? "," + item.playerA2 : ""} - 
+                 ${item.playerB1}
+                 ${item.type === "double" ? "," + item.playerB2 : ""}`}
+              </p>}
             </div>
             <GoSettings fontSize="1.5rem" />
           </div>
---
+++ src/containers/HomePage/GamesPage/Selector/Selector.jsx
@@ -0,0 +1,168 @@
+import { useState, useEffect } from "react";
+import "./Selector.scss";
+import Button from "../../../../components/UI/Button/Button"
+import { stringFa } from "../../../../assets/strings/stringFaCollection"
+import { useTheme } from "../../../../styles/ThemeProvider";
+import { useDispatch, useSelector } from "react-redux";
+import * as infoActions from "../../../../store/actions/setInfo"
+import { Navigate } from "react-router-dom";
+
+const Selector = (props) => {
+    const themeState = useTheme();
+    const theme = themeState.computedTheme;
+    const [options, setOptions] = useState([]);
+    const [title, setTitle] = useState(null);
+    const [index, setIndex] = useState(1);
+    const [max, setMax] = useState(2);
+    const [redirect, setRedirect] = useState(null);
+    const info = useSelector((state) => state.info);
+
+    const dispatch = useDispatch();
+    const setChosen = (items) => {
+        dispatch(infoActions.setChosen(items));
+    };
+
+    useEffect(() => {
+        if (info.team1.players.length === 1) {
+            setMax(2);
+        } else {
+            setMax(3);
+        }
+        setRedirect(null);
+    }, [])
+
+    useEffect(() => {
+        switch (index) {
+            case 1:
+                setTitle("تیم سمت راست خود را انتخاب کنید");
+                setOptions([{
+                    id: info.team1.players[0].id,
+                    text: (info.team1.players.map((player) => player.name)).join(" - "),
+                    selected: false
+                }, {
+                    id: info.team2.players[0].id,
+                    text: (info.team2.players.map((player) => player.name)).join(" - "),
+                    selected: false
+                },
+                ])
+                break;
+            case 2:
+                let players;
+                setTitle("زننده سرویس را انتخاب کنید");
+                players = [{
+                    id: info.team1.players[0].id,
+                    text: info.team1.players[0].name,
+                    selected: false
+                }]
+                if (max === 3)
+                    players = [...players, {
+                        id: info.team1.players[1].id,
+                        text: info.team1.players[1].name,
+                        selected: false
+                    }]
+                players = [...players, {
+                    id: info.team2.players[0].id,
+                    text: info.team2.players[0].name,
+                    selected: false
+                }]
+                if (max === 3)
+                    players = [...players, {
+                        id: info.team2.players[1].id,
+                        text: info.team2.players[1].name,
+                        selected: false
+                    }]
+                setOptions(players);
+                break;
+            case 3:
+                if (max === 3) {
+                    let players;
+                    setTitle("دریافت کننده را مشخص کنید");
+                    if (info.team1.server === 0) {
+                        players = info.team1.players.map((player) =>
+                            ({ id: player.id, text: player.name, selected: false })
+                        );
+                    } else {
+                        players = info.team2.players.map((player) =>
+                            ({ id: player.id, text: player.name, selected: false })
+                        );
+                    }
+                    setOptions(players);
+                }
+                break;
+            default:
+                setOptions([{ text: "درحال بارگزاری" }])
+                break;
+        }
+    }, [index]);
+
+    const optionClick = (i) => {
+        let updatedOptions = [...options];
+        updatedOptions.forEach(option => {
+            option.selected = false;
+        });
+        updatedOptions[i].selected = true;
+        setOptions(updatedOptions);
+        info.team1.players.forEach(player => {
+            if (player.id === options[i].id) {
+                setChosen({ index, id: player.id });
+            }
+        });
+        info.team2.players.forEach(player => {
+            if (player.id === options[i].id) {
+                setChosen({ index, id: player.id });
+            }
+        });
+        nextClick();
+    }
+
+    const nextClick = () => {
+        let valid = false;
+        options.forEach(item => {
+            if (item.selected) {
+                valid = true;
+            };
+        });
+        if (valid) {
+            if (index < max)
+                setIndex(index + 1);
+            else {
+                setRedirect(<Navigate to="/scoreboard" />)
+            }
+        } else {
+            alert("لطفا یک گزینه را انتخاب کنید");
+        }
+    }
+
+    const backClick = () => {
+        if (index > 1)
+            setIndex(index - 1);
+        if (index === 1)
+            props.show(false);
+    }
+
+    return (
+        <div className="selector-component">
+            {redirect}
+            <p className="title">{title}</p>
+            <div className="options-container">
+                {options.map((item, i) =>
+                    <div key={i} className="option-box" onClick={() => optionClick(i)}
+                        style={{
+                            borderColor: theme.primary,
+                            backgroundColor: item.selected ? theme.primary : "transparent",
+                            color: item.selected ? theme.on_primary : theme.on_background
+                        }}
+                    >
+                        {item.text}
+                    </div>
+                )}
+            </div>
+            <div className="action-btns">
+                <p className="prev" onClick={backClick}>بازگشت</p>
+                <Button onClick={nextClick}>{index === max ? "ذخیره" : stringFa.next}</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Selector
