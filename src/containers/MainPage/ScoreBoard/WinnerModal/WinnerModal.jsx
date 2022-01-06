2ded685cb11e47d2e1dc1559fffdead98ef821e6
lrdhosssein <77486783+lrdhosssein@users.noreply.github.com>
2022-01-06 19:33:04 +0330

    show all scores in winner modal - timer skip bug - R & S in second set in table

--- src/containers/MainPage/ScoreBoard/WinnerModal/WinnerModal.jsx
+++ src/containers/MainPage/ScoreBoard/WinnerModal/WinnerModal.jsx
@@ -11,21 +11,35 @@ const WinnerModal = (props) => {
     return (
         <div className="winner-modal-wrapper">
             <div className="details" style={{ flexDirection: info.team1.isRightTeam ? "row-reverse" : "row" }}>
-                <div className={`team-block ${props.teamWon === "team1" && "winner"}`}
+                <div className={`team-block 
+                ${info.team1.isRightTeam ? "rev" : ""}
+                ${props.teamWon === "team1" && "winner"}`}
                     style={{ color: props.teamWon === "team1" ? theme.primary : theme.on_background }}
                 >
                     <div className="players-section">
                         {info.team1.players.map((p, i) => <p className="names" key={i}>{p.name}</p>)}
                     </div>
-                    <div className="set-score">{info.team1.setWon}</div>
+                    <div className="set-score">
+                        <p>{info.team1.setWon}</p>
+                        <p className="scores-details">
+                            {info.team1.scores.map((s) => <p>{s}</p>)}
+                        </p>
+                    </div>
                 </div>
-                <div className={`team-block rev ${props.teamWon === "team2" && "winner"}`}
+                <div className={`team-block 
+                ${info.team2.isRightTeam ? "rev" : ""}
+                ${props.teamWon === "team2" && "winner"}`}
                     style={{ color: props.teamWon === "team2" ? theme.primary : theme.on_background }}
                 >
                     <div className="players-section">
                         {info.team2.players.map((p, i) => <p className="names" key={i}>{p.name}</p>)}
                     </div>
-                    <div className="set-score">{info.team2.setWon}</div>
+                    <div className="set-score">
+                        <p>{info.team2.setWon}</p>
+                        <p className="scores-details">
+                            {info.team2.scores.map((s) => <p>{s}</p>)}
+                        </p>
+                    </div>
                 </div>
             </div>
             <Button onClick={() => window.location.reload(false)}> تایید </Button>
