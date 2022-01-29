7bdf767338b80a328790c3e4162f697ee05c36a0
mohammadshayei <mmdshayei78@gmail.com>
2022-01-29 11:21:43 +0330

    service woroker edired

--- src/service-worker.js
+++ src/service-worker.js
@@ -68,5 +68,19 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
-
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 // Any other custom service worker logic can go here.
