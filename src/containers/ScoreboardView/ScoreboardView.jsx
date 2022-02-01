a75a35700450fa390405070a334780648e2adfde
mohammadshayei <mmdshayei78@gmail.com>
2022-02-01 16:16:15 +0330

    land number

--- src/containers/ScoreboardView/ScoreboardView.jsx
+++ src/containers/ScoreboardView/ScoreboardView.jsx
@@ -16,7 +16,7 @@ const ScoreboardView = () => {
     const token = useSelector(state => state.auth.token)
     let navigate = useNavigate();
 
-
+    console.log(game)
     useEffect(() => {
         if (game) {
             let updatedData = {}
@@ -107,95 +107,100 @@ const ScoreboardView = () => {
                 {data ?
                     Object.entries(data).map(([k, v]) =>
                         (v.players.length > 0) &&
-                        (<div
-                            className={`player-block-view ${k === "teamB" && "rev-block"}`}
-                            style={{
-                                background: (data.teamA.setWon === 2) || (data.teamB.setWon === 2) ?
-                                    v.setWon === 2 ?
-                                        "rgba(172, 209, 175,0.25)" :
-                                        "rgba(244, 113, 116,0.3)"
-                                    :
-                                    "transparent"
-                            }}
-                            key={k}
-                        >
-                            <div className="player-name-and-image"
-                                style={{ justifyContent: v.players.length === 1 ? "center" : "space-between" }}
+                        (
+                            <div
+                                className={`player-block-view ${k === "teamB" && "rev-block"}`}
+                                style={{
+                                    background: (data.teamA.setWon === 2) || (data.teamB.setWon === 2) ?
+                                        v.setWon === 2 ?
+                                            "rgba(172, 209, 175,0.25)" :
+                                            "rgba(244, 113, 116,0.3)"
+                                        :
+                                        "transparent"
+                                }}
+                                key={k}
                             >
-                                <div className="player-name"
-                                    style={{
-                                        transform:
-                                            v.players[0].player.username.length > 16 ?
-                                                (`translateX(${k === "teamB" ? "-" : "+"}5vw)`) :
-                                                "translateX(0)"
-                                    }}
+                                <div className="player-name-and-image"
+                                    style={{ justifyContent: v.players.length === 1 ? "center" : "space-between" }}
                                 >
-                                    {v.players[0].player.username}
-                                </div>
-                                <img className="player-img"
-                                    src={v.players[0].player.image !== '' ?
-                                        `${baseUrl}uploads/players/${v.players[0].player.image}` :
-                                        PROFILE_IMAGE}
-                                    alt="profile_image"
-                                    style={{
-                                        maxWidth: v.players.length === 1 ? "80%" : "50%",
-                                        marginTop: v.players.length === 1 ? "1.5rem" : "0"
-                                    }}
-                                />
-                                {v.players[1] &&
-                                    <img
-                                        className="player-img"
-                                        src={v.players[1].player.image !== '' ?
-                                            `${baseUrl}uploads/players/${v.players[1].player.image}` :
+                                    <div className="player-name"
+                                        style={{
+                                            transform:
+                                                v.players[0].player.username.length > 16 ?
+                                                    (`translateX(${k === "teamB" ? "-" : "+"}5vw)`) :
+                                                    "translateX(0)"
+                                        }}
+                                    >
+                                        {v.players[0].player.username}
+                                    </div>
+                                    <img className="player-img"
+                                        src={v.players[0].player.image !== '' ?
+                                            `${baseUrl}uploads/players/${v.players[0].player.image}` :
                                             PROFILE_IMAGE}
-                                        alt="profile_image" />
-                                }
-                                {v.players[1] && <div className="player-name"
-                                    style={{
-                                        transform:
-                                            v.players[1].player.username.length > 16 ?
-                                                (`translateX(${k === "teamB" ? "-" : "+"}5vw`) :
-                                                "translateX(0)"
-                                    }}
-                                >
-                                    {v.players[1].player.username}
-                                </div>}
-                            </div>
-                            <div className="player-score-and-set">
-                                <div className="set-score"
-                                    style={{
-                                        fontSize: (data.teamA.setWon === 2) || (data.teamB.setWon === 2) ? "13vw" : "8vw",
-                                    }}
-                                >
-                                    <div>{v.setWon}</div>
+                                        alt="profile_image"
+                                        style={{
+                                            maxWidth: v.players.length === 1 ? "80%" : "50%",
+                                            marginTop: v.players.length === 1 ? "1.5rem" : "0"
+                                        }}
+                                    />
+                                    {v.players[1] &&
+                                        <img
+                                            className="player-img"
+                                            src={v.players[1].player.image !== '' ?
+                                                `${baseUrl}uploads/players/${v.players[1].player.image}` :
+                                                PROFILE_IMAGE}
+                                            alt="profile_image" />
+                                    }
+                                    {v.players[1] && <div className="player-name"
+                                        style={{
+                                            transform:
+                                                v.players[1].player.username.length > 16 ?
+                                                    (`translateX(${k === "teamB" ? "-" : "+"}5vw`) :
+                                                    "translateX(0)"
+                                        }}
+                                    >
+                                        {v.players[1].player.username}
+                                    </div>}
                                 </div>
-                                {gameScores ?
-                                    <div className="score-digit">
-                                        <div className="scores"
-                                            style={{
-                                                fontSize: "8vw",
-                                                lineHeight: "8vw"
-                                            }}
-                                        >{gameScores[k].map((item) =>
-                                            <p>{item}</p>
-                                        )}</div>
+                                <div className="player-score-and-set">
+                                    <div className="set-score"
+                                        style={{
+                                            fontSize: (data.teamA.setWon === 2) || (data.teamB.setWon === 2) ? "13vw" : "8vw",
+                                        }}
+                                    >
+                                        <div>{v.setWon}</div>
                                     </div>
-                                    :
-                                    <div className="score-digit">
-                                        <div className="digital-panel"
-                                            style={{
-                                                fontSize: v.score > 9 ? "16vw" : "20vw",
-                                                lineHeight: v.score > 9 ? "16vw" : "15vw"
-                                            }}
-                                        >{v.score}</div>
-                                    </div>
-                                }
+                                    {gameScores ?
+                                        <div className="score-digit">
+                                            <div className="scores"
+                                                style={{
+                                                    fontSize: "8vw",
+                                                    lineHeight: "8vw"
+                                                }}
+                                            >{gameScores[k].map((item) =>
+                                                <p>{item}</p>
+                                            )}</div>
+                                        </div>
+                                        :
+                                        <div className="score-digit">
+                                            <div className="digital-panel"
+                                                style={{
+                                                    fontSize: v.score > 9 ? "16vw" : "20vw",
+                                                    lineHeight: v.score > 9 ? "16vw" : "15vw"
+                                                }}
+                                            >{v.score}</div>
+                                        </div>
+                                    }
+                                </div>
                             </div>
-                        </div>
 
                         )
                     )
                     : "Loading Info..."}
+                <div className="detail-scoreboardview">
+                    <p className="detail-landnumber">شماره زمین : {game.land_number}</p>
+                    <p>شماره بازی : {game.game_number}</p>
+                </div>
             </div>
         </div>
     )
