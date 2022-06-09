e5d0d07b38dd97156e8e6904cc1be874f8e24597
lrdhosssein <ph.ramires@yahoo.com>
2022-06-09 11:45:24 +0430

    game item player names fixed

--- src/containers/HomePage/TournamentPage/TournamentContentSection/Item/GameItem.jsx
+++ src/containers/HomePage/TournamentPage/TournamentContentSection/Item/GameItem.jsx
@@ -26,7 +26,8 @@ const GameItem = (props) => {
                                 theme.on_primary :
                                 theme.on_background,
                     }}
-                >{props.teamA.players.map((player, i) => <p key={i} className="name">{player.player.username}</p>)}</div>
+                >{props.teamA.players.map((player, i) =>
+                    <div title={player.player.username} key={i} className="name">{player.player.username}</div>)}</div>
                 <p className="score">{`${props.teamA.setWon} - ${props.teamB.setWon}`}</p>
                 <div className="names"
                     style={{
@@ -36,7 +37,8 @@ const GameItem = (props) => {
                                 theme.on_primary :
                                 theme.on_background,
                     }}
-                >{props.teamB.players.map((player, i) => <p key={i} className="name">{player.player.username}</p>)}</div>
+                >{props.teamB.players.map((player, i) =>
+                    <div title={player.player.username} key={i} className="name">{player.player.username}</div>)}</div>
             </div>
             <div className="game-details">{`شماره زمین : ${props.land_number} - شماره بازی : ${props.game_number}`}</div>
             {props.status === 1 &&
