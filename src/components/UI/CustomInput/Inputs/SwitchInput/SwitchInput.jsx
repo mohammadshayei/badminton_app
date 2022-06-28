98f41c583cca3bad9b8a8375b39a766ba8e32345
lrdhosssein <ph.ramires@yahoo.com>
2022-06-28 17:09:23 +0430

    retouch components

--- src/components/UI/CustomInput/Inputs/SwitchInput/SwitchInput.jsx
+++ src/components/UI/CustomInput/Inputs/SwitchInput/SwitchInput.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './SwitchInput.scss'
 import Switch from "react-switch";
+import { useTheme } from '../../../../../styles/ThemeProvider';
 
 const SwitchInput = (props) => {
+    const themeState = useTheme();
+    const theme = themeState.computedTheme;
+
     return (
         <div className='switch-input-container'>
             <p className='switch-input-title'>{props.title}</p>
-            <Switch onChange={props.onChange} checked={props.value} />
+            <Switch
+                onChange={props.onChange}
+                checked={props.value}
+                offColor='#999'
+                onColor={theme.primary}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                handleDiameter={18}
+                height={20}
+                width={40}
+            />
         </div>
     )
 }
