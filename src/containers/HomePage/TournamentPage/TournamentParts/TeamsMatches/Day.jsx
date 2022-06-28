dcf65515d876e3de58b0221e8626a3f3baf0fef6
mohammadshayei <mmdshayei78@gmail.com>
2022-06-28 14:01:13 +0430

    convert date to readable date

--- src/containers/HomePage/TournamentPage/TournamentParts/TeamsMatches/Day.jsx
+++ src/containers/HomePage/TournamentPage/TournamentParts/TeamsMatches/Day.jsx
@@ -1,9 +1,18 @@
 import "./TeamsMatches.scss"
 import { useTheme } from "../../../../../styles/ThemeProvider";
+import { useEffect, useState } from "react";
+import { getDateInfo } from "../../../../../utils/funcs";
 
-const Day = ({ selectDay, day, dayKey }) => {
+const Day = ({ selectDay, day }) => {
+    const [dateInfo, setDateInfo] = useState(null)
     const themeState = useTheme();
     const theme = themeState.computedTheme;
+
+    useEffect(() => {
+        if (!day.date) return;
+        console.log('here')
+        setDateInfo(getDateInfo(new Date(day.date)))
+    }, [day.date])
 
     return <div
         className="day"
@@ -13,23 +22,23 @@ const Day = ({ selectDay, day, dayKey }) => {
             // transform: day.selected ? "scale(1.05)" : "scale(1)",
             opacity: day.selected ? 1 : 0.7
         }}
-        onClick={() => selectDay(dayKey)}
+        onClick={selectDay}
     >
         <div className="day-head"
             style={{
                 backgroundColor: day.selected ? theme.primary : theme.darken_border_color,
                 color: theme.on_primary
             }}
-        >{day.date.length > 0 ? 'شنبه' : 'روز'}</div>
+        >{dateInfo ? dateInfo.dayName : 'روز'}</div>
         <div className="day-body"
             style={{
                 backgroundColor: day.selected ? theme.surface : "transparent",
             }}
         >
-            {day.date.length > 0 ?
-                <p className="day-digit">1</p> :
+            {dateInfo ?
+                <p className="day-digit">{dateInfo.dayCount}</p> :
                 day.counter}
-            <p>{day.date.length > 0 ? 'مرداد' : ""}</p>
+            <p>{dateInfo ? dateInfo.month : ""}</p>
         </div>
     </div>;
 };
