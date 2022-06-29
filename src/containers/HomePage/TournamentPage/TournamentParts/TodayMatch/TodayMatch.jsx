7fd1dfc96a034788b76707e4483ee3105aff27f6
lrdhosssein <ph.ramires@yahoo.com>
2022-06-29 15:35:13 +0430

    today match responsiveness

--- src/containers/HomePage/TournamentPage/TournamentParts/TodayMatch/TodayMatch.jsx
+++ src/containers/HomePage/TournamentPage/TournamentParts/TodayMatch/TodayMatch.jsx
@@ -3,26 +3,27 @@ import "./TodayMatch.scss"
 import { stringFa } from "../../../../../assets/strings/stringFaCollection";
 import CustomInput, { elementTypes } from "../../../../../components/UI/CustomInput/CustomInput";
 import { useTheme } from "../../../../../styles/ThemeProvider";
+import TransparentButton from "../../../../../components/UI/Button/TransparentButton/TransparentButton";
 
 const TodayMatch = () => {
-    const [games, setGames] = useState({
-        game1: {
-            index: "انفرادی اول",
-            team1: { players: { player1: { name: "محمد محمدی", image: "" } } }
-            , team2: { players: { player1: { name: "احمد احمدی", image: "" } } }
-        },
-        game2: {
-            index: "انفرادی دوم",
-            team1: { players: { player1: { name: "", image: "" } } }
-            , team2: { players: { player1: { name: "", image: "" } } }
-        }
+    const [games, setGames] = useState([
+        [
+            "انفرادی اول",
+            { teamName: "رعد پدافند هوایی قم", players: { player1: { name: "محمد محمدی", image: "" } } }
+            , { teamName: "پروتئین 99 البرز", players: { player1: { name: "احمد احمدی", image: "" } } }
+        ],
+        [
+            "انفرادی دوم",
+            { teamName: "رعد پدافند هوایی قم", players: { player1: { name: "", image: "" } } }
+            , { teamName: "پروتئین 99 البرز", players: { player1: { name: "", image: "" } } }
+        ]
         ,
-        game3: {
-            index: "دونفره اول",
-            team1: { players: { player1: { name: "حسن حسنی", image: "" }, player2: { name: "کریم کریمی", image: "" } } }
-            , team2: { players: { player1: { name: "داوود داوودی", image: "" }, player2: { name: "جواد جوادی", image: "" } } }
-        }
-    });
+        [
+            "دونفره اول",
+            { teamName: "رعد پدافند هوایی قم", players: { player1: { name: "حسن حسنی", image: "" }, player2: { name: "کریم کریمی", image: "" } } }
+            , { teamName: "پروتئین 99 البرز", players: { player1: { name: "داوود داوودی", image: "" }, player2: { name: "جواد جوادی", image: "" } } }
+        ]
+    ]);
 
     const themeState = useTheme();
     const theme = themeState.computedTheme;
@@ -39,56 +40,87 @@ const TodayMatch = () => {
                             elementType={elementTypes.dropDown}
                             items={[]}
                             inputContainer={{ padding: "0" }}
-                            containerStyle={{ fontSize: "0.7rem" }}
                         />
                     </div>
                     <div className="date">1401/05/20</div>
                 </div>
-                {Object.entries(games).map(([k1, game]) =>
+                {games.map((game, k1) =>
                     <div key={k1} className="match-game"
-                        style={{ borderTop: k1 !== "game1" ? `1px solid ${theme.border_color}` : "none" }}
+                        style={{
+                            backgroundColor: theme.surface
+                        }}
                     >
-                        {console.log(k1)}
-                        <div className="match-game-index"
-                            style={{ backgroundColor: theme.surface }}
-                        >{game.index}</div>
-                        <div className="match-game-number">
-                            <CustomInput
-                                inputStyle={{ fontSize: "0.7rem", }}
-                                inputContainer={{ padding: "0" }}
-                                elementConfig={{
-                                    placeholder: stringFa.number_of_game
+                        <div className="match-game-header">
+                            <div className="match-game-number">
+                                <CustomInput
+                                    elementConfig={{
+                                        placeholder: stringFa.number_of_game,
+                                    }}
+                                    inputContainer={{
+                                        padding: "0",
+                                        width: "100px",
+                                    }}
+                                    inputStyle={{
+                                        fontSize: "clamp(0.7rem, 1.5vw, 0.9rem)",
+                                        minWidth: "100px",
+                                        direction: "ltr"
+                                    }}
+                                />
+                            </div>
+                            <div className="match-game-index"
+                                style={{
+                                    backgroundColor: theme.darken_border_color, //if done -> theme.primary
+                                    color: theme.on_primary
                                 }}
-                            />
+                            >{game[0]}</div>
+                            <div className="match-game-number  game-court">
+                                <CustomInput
+                                    elementConfig={{
+                                        placeholder: "court",
+                                    }}
+                                    inputContainer={{
+                                        padding: "0",
+                                        width: "100px",
+                                    }}
+                                    inputStyle={{
+                                        fontSize: "clamp(0.7rem, 1.5vw, 0.9rem)",
+                                        minWidth: "100px",
+                                        direction: "ltr"
+                                    }}
+                                />
+                            </div>
                         </div>
                         <div className="match-game-details">
-                            {Object.entries(game).map(([k2, team]) =>
-                                k2 !== "index" &&
-                                <>
-                                    <div key={k2} className={`match-game-team ${k2 === "team2" ? "left" : ''}`}>
-                                        <div className="team-name">
-                                            {k2}
-                                        </div>
-                                        <div className="players">
-                                            {Object.entries(team.players).map(([k3, player]) =>
-                                                <div key={k3} className="player">
-                                                    <div className="player-name">
-                                                        <CustomInput
-                                                            placeHolder={stringFa.undefined}
-                                                            elementType={elementTypes.dropDown}
-                                                            items={[]}
-                                                            inputContainer={{ padding: "0" }}
-                                                            containerStyle={{ fontSize: "0.7rem" }}
-                                                        />
-                                                    </div>
-                                                </div>
-                                            )}
-                                        </div>
+                            {game.map((team, k2) =>
+                                k2 > 0 &&
+                                <div key={k2} className={`match-game-team ${k2 === 2 ? "left" : ''}`}>
+                                    <div className="team-name">
+                                        {team.teamName}
                                     </div>
-                                    {k2 === "team1" ? <p className="dash">-</p> : ''}
-                                </>
+                                    <div className="players">
+                                        {Object.entries(team.players).map(([k3, player]) =>
+                                            <div key={k3} className="player">
+                                                <CustomInput
+                                                    placeHolder={stringFa.undefined}
+                                                    elementType={elementTypes.dropDown}
+                                                    items={[]}
+                                                    inputContainer={{ padding: "0" }}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                    {/* {k2 === 1 ? <p className="dash">-</p> : ''} */}
+                                </div>
                             )}
                         </div>
+                        <TransparentButton
+                            config={{ disabled: true }}
+                            ButtonStyle={{
+                                padding: "0",
+                                fontSize: "clamp(0.8rem,1vw,0.9rem)"
+                            }}>
+                            {stringFa.save}
+                        </TransparentButton>
                     </div>)}
             </>
         }
