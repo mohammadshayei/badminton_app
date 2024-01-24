df888fbd1eb5d78bffebe9e3ce17f21fa2989fce
Hosssein <77486783+lrdhosssein@users.noreply.github.com>
2024-01-25 01:23:03 +0330

    remove radio image button animation

--- src/components/UI/CustomInput/Inputs/RadioImageButton/RadioImageButton.jsx
+++ src/components/UI/CustomInput/Inputs/RadioImageButton/RadioImageButton.jsx
@@ -1,5 +1,5 @@
 import "./RadioImageButton.scss"
-import { animated, useSpring } from "react-spring";
+// import { animated, useSpring } from "react-spring";
 import { useTheme } from "../../../../../styles/ThemeProvider";
 import rocket from "../../../../../assets/images/rocket.png";
 import rockets from "../../../../../assets/images/rockets.png";
@@ -7,23 +7,23 @@ import rockets from "../../../../../assets/images/rockets.png";
 const RadioImageButton = (props) => {
     const themeState = useTheme();
     const theme = themeState.computedTheme;
-    const styles = useSpring({
-        from: { left: props.double ? "50%" : "0" },
-        to: { left: props.double ? "0" : "50%" },
-        config: { mass: 0.5, tension: 500, friction: 20 },
-    });
+    // const styles = useSpring({
+    //     from: { left: props.double ? "50%" : "0" },
+    //     to: { left: props.double ? "0" : "50%" },
+    //     config: { mass: 0.5, tension: 500, friction: 20 },
+    // });
 
     return (
         <div className="radio-btn-container">
             <p className="title-class-name">{props.title}</p>
             <div className="width-fixer">
-                <animated.div
+                <div
                     className="selector"
                     style={{
-                        ...styles,
+                        // ...styles,
                         background: theme.primary,
                     }}
-                ></animated.div>
+                ></div>
                 <img src={rocket} alt="rocket" onClick={() => props.onChange(false)} />
                 <img src={rockets} alt="rocket" onClick={() => props.onChange(true)} />
             </div>
