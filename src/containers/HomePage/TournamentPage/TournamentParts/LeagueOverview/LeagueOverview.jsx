dc8d1beb9a95df134e17d7713e5037566ea89f69
lrdhossein <ph.ramires@yahoo.com>
2026-02-25 11:47:02 +0330

    update league table

--- src/containers/HomePage/TournamentPage/TournamentParts/LeagueOverview/LeagueOverview.jsx
+++ src/containers/HomePage/TournamentPage/TournamentParts/LeagueOverview/LeagueOverview.jsx
@@ -11,22 +11,54 @@ import DEFAULT_LOGO from '../../../../../assets/images/team_avatar.png';
 const LeagueOverview = ({ tournamentId }) => {
     const [contentLoading, setContentLoading] = useState(false);
     const [dialog, setDialog] = useState(null);
-    const [data, setData] = useState(
-        [
+    const [data, setData] = useState([]);
+
+    const { token } = useSelector(state => state.auth)
+
+    const location = useLocation()
+    const searchParams = new URLSearchParams(location.search);
+    const part = searchParams.get("part");
+
+    useEffect(() => {
+        if (!tournamentId || !part) return;
+        const tournaments = ["58d9563e01e14005898d9ce96be9fd06", "1e69a30fbb3b48c8bbb82be7fb495fb3"];
+        if (tournaments.includes(tournamentId)) return;
+        (async () => {
+            try {
+                setContentLoading(true)
+                let fetchedItems = await dynamicApi({ id: tournamentId }, token, `get_league_table`)
+                if (!fetchedItems.success) {
+                    setDialog(<ErrorDialog type="error">{fetchedItems.data.message}</ErrorDialog>)
+                    return;
+                }
+                setData(fetchedItems.data.data?.filter(team => !team._id?.name?.includes("محلات")))
+            } catch (error) {
+                setContentLoading(false)
+                setDialog(<ErrorDialog type="error">{stringFa.error_occured}</ErrorDialog>)
+            }
+            setContentLoading(false)
+
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tournamentId, part])
+
+    useEffect(() => {
+      if (tournamentId === "1e69a30fbb3b48c8bbb82be7fb495fb3") {
+        setData([
             {
                 "_id": {
                     "_id": "fdf3c9fe5a1f43aa82f6fd42546b7b17",
                     "name": "کاژه تهران",
                     "image": "image-1727518998573.png"
                 },
-                "gameWon": 31,
+                "gameWon": 36,
                 "gameLost": 9,
-                "matchsCount": 8,
-                "matchWon": 7,
+                "matchsCount": 9,
+                "matchWon": 8,
                 "matchLost": 1,
-                "scoreLost": 1345,
-                "scored": 1751,
-                "setWon": 67,
+                "scoreLost": 1451,
+                "scored": 1961,
+                "setWon": 77,
                 "setLost": 22
             },
             {
@@ -35,15 +67,15 @@ const LeagueOverview = ({ tournamentId }) => {
                     "name": " بسکی گنبد",
                     "image": "image-1763651814455.png"
                 },
-                "gameWon": 30,
-                "gameLost": 10,
-                "matchsCount": 8,
-                "matchWon": 7,
+                "gameWon": 34,
+                "gameLost": 11,
+                "matchsCount": 9,
+                "matchWon": 8,
                 "matchLost": 1,
-                "scoreLost": 1278,
-                "scored": 1708,
-                "setWon": 63,
-                "setLost": 25
+                "scoreLost": 1448,
+                "scored": 1910,
+                "setWon": 71,
+                "setLost": 28
             },
             {
                 "_id": {
@@ -51,15 +83,15 @@ const LeagueOverview = ({ tournamentId }) => {
                     "name": "چادرملو اردکان",
                     "image": "image-1663232613997.png"
                 },
-                "gameWon": 20,
-                "gameLost": 20,
-                "matchsCount": 8,
+                "gameWon": 21,
+                "gameLost": 24,
+                "matchsCount": 9,
                 "matchWon": 4,
-                "matchLost": 4,
-                "scoreLost": 1671,
-                "scored": 1626,
-                "setWon": 44,
-                "setLost": 47
+                "matchLost": 5,
+                "scoreLost": 1873,
+                "scored": 1796,
+                "setWon": 47,
+                "setLost": 55
             },
             {
                 "_id": {
@@ -100,44 +132,86 @@ const LeagueOverview = ({ tournamentId }) => {
                     "image": "image-1694518044936.jpg"
                 },
                 "gameWon": 8,
-                "gameLost": 27,
-                "matchsCount": 7,
+                "gameLost": 32,
+                "matchsCount": 8,
                 "matchWon": 0,
-                "matchLost": 7,
-                "scoreLost": 1464,
-                "scored": 1160,
+                "matchLost": 8,
+                "scoreLost": 1674,
+                "scored": 1266,
                 "setWon": 20,
-                "setLost": 56
+                "setLost": 66
             }
-        ]
-    );
-
-    const { token } = useSelector(state => state.auth)
-
-    const location = useLocation()
-    const searchParams = new URLSearchParams(location.search);
-    const part = searchParams.get("part");
-
-    // useEffect(() => {
-    //     if (!tournamentId || !part) return;
-    //     (async () => {
-    //         try {
-    //             setContentLoading(true)
-    //             let fetchedItems = await dynamicApi({ id: tournamentId }, token, `get_league_table`)
-    //             if (!fetchedItems.success) {
-    //                 setDialog(<ErrorDialog type="error">{fetchedItems.data.message}</ErrorDialog>)
-    //                 return;
-    //             }
-    //             setData(fetchedItems.data.data?.filter(team => !team._id?.name?.includes("محلات")))
-    //         } catch (error) {
-    //             setContentLoading(false)
-    //             setDialog(<ErrorDialog type="error">{stringFa.error_occured}</ErrorDialog>)
-    //         }
-    //         setContentLoading(false)
-
-    //     })()
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [tournamentId, part])
+        ])
+      } else if (tournamentId === "58d9563e01e14005898d9ce96be9fd06"){
+        setData([
+            {
+                "_id": {
+                    "_id": "5cd84aefe7a34bde857a4226b345af93",
+                    "name": "کاژه تهران (بانوان)",
+                    "image": "image-1762416169458.png"
+                },
+                "gameWon": 19,
+                "gameLost": 6,
+                "matchsCount": 5,
+                "matchWon": 4,
+                "matchLost": 1,
+                "scoreLost": 794,
+                "scored": 1051,
+                "setWon": 40,
+                "setLost": 14
+            },
+            {
+                "_id": {
+                    "_id": "b13c57bdc5ef4c939100cb8e5ac1e592",
+                    "name": "نفت تهران",
+                    "image": "image-1763651899676.JPG"
+                },
+                "gameWon": 17,
+                "gameLost": 8,
+                "matchsCount": 5,
+                "matchWon": 4,
+                "matchLost": 1,
+                "scoreLost": 908,
+                "scored": 1035,
+                "setWon": 36,
+                "setLost": 19
+            },
+            {
+                "_id": {
+                    "_id": "4852a4567baa4e8e91320c4c84197154",
+                    "name": "درخشان زنجان",
+                    "image": "image-1763651885866.JPG"
+                },
+                "gameWon": 8,
+                "gameLost": 17,
+                "matchsCount": 5,
+                "matchWon": 2,
+                "matchLost": 3,
+                "scoreLost": 980,
+                "scored": 874,
+                "setWon": 17,
+                "setLost": 36
+            },
+            {
+                "_id": {
+                    "_id": "5fc2e57445a84f32b43a6e20670706f6",
+                    "name": "شهید فخار مشهد",
+                    "image": "image-1663232548749.PNG"
+                },
+                "gameWon": 6,
+                "gameLost": 19,
+                "matchsCount": 5,
+                "matchWon": 0,
+                "matchLost": 5,
+                "scoreLost": 1079,
+                "scored": 801,
+                "setWon": 16,
+                "setLost": 40
+            }
+        ])
+      }
+    }, [tournamentId])
+    
 
     return <div className="league-overview">
         <table className="league-table">
